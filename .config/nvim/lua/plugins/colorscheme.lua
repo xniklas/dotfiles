d414a1ac16ce7074a3d08d6a0230d7a926f3813d
Niklas <niklas.loebermann@proton.me>
2025-06-18 12:43:06 +0200

    changed yadm alt files, add easyeffects presets, changed kitty theme, changed neovim theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,15 @@
 return {
-  -- add catppuccin
-  { "catppuccin/nvim" },
-  { "Mofiqul/dracula.nvim" },
+    -- add catppuccin
+    { "catppuccin/nvim" },
+    { "ficcdaf/ashen.nvim" },
 
-  {
-    dir = "/home/raydn/dev/neovim/strix.nvim",
-    name = "strix",
-    lazy = false,
-    priority = 1000, -- Load before other plugins
-    -- config = function()
-    --   vim.cmd("colorscheme strix-night")
-    -- end,
-  },
-
-  -- Configure LazyVim to load catppuccin
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      -- colorscheme = "catppuccin",
-      -- colorscheme = "strix-night",
-      -- colorscheme = "dracula",
-      colorscheme = "tokyonight-night",
+    -- Configure LazyVim to load catppuccin
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            -- colorscheme = "catppuccin",
+            -- colorscheme = "tokyonight-night",
+            colorscheme = "ashen",
+        },
     },
-  },
 }
